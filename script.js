0d5d0ad2ddc63679aa539273d83e6a840099d8ca
Sukruth Rao <sukruthrao@MacBookPro.lan>
2026-03-18 10:33:12 -0700

    Add repo link for inpainting artifacts project

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const projects = [
       "Study of how image inpainting artifacts influence downstream language behavior in vision-language systems.",
     href: "https://arxiv.org/abs/2602.20520",
     linkLabel: "Publication",
+    docHref: "https://github.com/raosukruth/inpainting-artifacts-language",
+    docLabel: "Repo",
     tags: ["Diffusion Models", "VLMs", "Robustness"]
   },
   {

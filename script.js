5cea9eea952162bdea0fa731184c7524abe9ee86
Sukruth Rao <sukruthrao@MacBookPro.lan>
2026-03-18 11:32:52 -0700

    Remove robustness tag from inpainting project

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const projects = [
     linkLabel: "Repo",
     docHref: "https://arxiv.org/abs/2602.20520",
     docLabel: "Preprint",
-    tags: ["Diffusion Models", "VLMs", "Robustness"]
+    tags: ["Diffusion Models", "VLMs"]
   },
   {
     title: "IRES Comic Mischief",
